305d3d29ace099d991a74a1f7d769ba567dbd0b2
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-03-20 21:19:07 -0700

    made database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,20 +1,38 @@
 from flask import Flask 
 
+
+from flask_migrate import Migrate
+
+
 #factory
 def create_app(): 
     app = Flask(__name__)
+
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:new_password@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate (app, models.db)
 
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax!'
 
     #register pet blueprint
-    from . import facts
-    app.register_blueprint(facts.bp)
+    from . import fact
+    app.register_blueprint(fact.bp)
     from . import pet
     app.register_blueprint(pet.bp)
     
     return app    
 
+
+
     # return the app
     return app
+
+
